e48e277e8e4779d2bc2cf4553aa47d496ffb62a6
devingrischow <devingrischow@outlook.com>
2023-12-17 19:56:59 -0600

    Initial Commit

---
+++ CustomProgressBarTests/CustomProgressBarTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomProgressBarTestsApp.swift
+//  CustomProgressBarTests
+//
+//  Created by Devin Grischow on 12/17/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomProgressBarTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
